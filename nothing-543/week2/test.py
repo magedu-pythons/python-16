213b328c2f7d9a55c94f54078ec0057b42452e24
unknown <401564281@qq.com>
2018-10-20 21:11:56 +0800

    nothing-543 week2

---
+++ nothing-543/week2/test.py
@@ -0,0 +1,18 @@
+import random
+list=[]
+for x in range(65,91):
+    a=str(chr(x)) 
+    list.append(a)
+for x in range(97,123):
+    a=str(chr(x))  
+    list.append(a)     
+for x in range(10):
+    list.append(str(x))
+def gen_code():
+    s=''
+    for x in range(16):
+        a=random.choice(list)
+        s=s+a
+    print s
+for x in range(200):
+    gen_code()
