233c8107b496585ea8e5a0fce742d09105b45832
liyd\andy <liyuandong163@163.com>
2018-10-30 23:07:25 +0800

    Remove duplicates promo code

--- Liyuandong/WEEK2/generatePromoCode.py
+++ Liyuandong/WEEK2/generatePromoCode.py
@@ -3,7 +3,9 @@
 
 # 使用random模块进行生成优惠券
 import random
+
 for i in range(200):
+    s = []
     L = []
     big_str = chr(random.randint(97, 122))
     little_str = chr(random.randint(65, 90))
@@ -11,22 +13,27 @@ for i in range(200):
     for i in range(6):
         promo_code = random.choice((big_str, little_str, num))
         L.append(promo_code)
-    print(''.join(L))
+    if ''.join(L) not in s:
+        s.append(''.join(L))
+    print(s)
 
 # 使用hashlib
+
 import hashlib, random
+
 for i in range(200):
+    s = []
     L = []
     for i in range(6):
         m = hashlib.md5(b'chr[random.randint(0, 126)]').hexdigest()
         promo_code = random.choice(m)
         L.append(promo_code)
-    print(''.join(L))
-
-
+    if ''.join(L) not in s:
+        s.append(''.join(L))
+    print(s)
 
 """
 (0 + 0)
-    
+
     优惠码没有考虑去重
 """
