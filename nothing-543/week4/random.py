7bfda800bbe058f13c79aaff5cd46a7077881829
nothin-543 <401564281@qq.com>
2018-11-06 18:49:53 +0800

    nothing-543 week4

---
+++ nothing-543/week4/random.py
@@ -0,0 +1,23 @@
+import random, copy
+low = int(input('give me a lower'))
+upp = int(input('give me a upper'))
+lst = [0] * 20
+for i in range(20):
+    lst[i] = random.randint(low,upp)
+print(lst)
+lst2 = copy.copy(lst)
+lst1 = [0] * 3
+for j in range(3):
+    maxnum = 0
+    for n in range(len(lst2)):
+        if maxnum > lst2[n]:
+            continue
+        else:
+            maxnum = lst2[n]
+            index = n
+    lst2[index] = 0
+    lst1[j] = index
+for i in lst1:
+    print(lst[i])
+    #print(maxnum)
+    #lst.remove(maxnum)
