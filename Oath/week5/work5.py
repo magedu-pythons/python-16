6056075e1698660d3d91f8401e64f8b669ef9f76
HokYng <42990816+HokYng@users.noreply.github.com>
2018-11-23 20:29:09 +0800

    week5

---
+++ Oath/week5/work5.py
@@ -0,0 +1,22 @@
+# 打乱一个排好序的list对象alist
+import random
+
+def shuffle_lst(lst:list) -> list:
+	random.shuffle(lst)
+	return lst
+array=[1, 2, 3, 4, 5]
+print(shuffle_lst(array))
+
+# 随机返回一种商品
+def random_products(store:dict, item=[]):
+	if not item:
+		for k, v in store.items():
+			item += [k] * v
+	goods = random.choice(item)
+	item.remove(goods)
+	store[goods] -= 1
+	if not store[goods]:
+		store.pop(goods)
+	return goods
+store={'socks':10, 'shoe':20, 'slipper':30, 'necklace':40}
+print(random_products(store))
