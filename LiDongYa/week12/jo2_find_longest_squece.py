b82eee735877241680feae81adc4c1034010726b
dFie <40017663+dFie@users.noreply.github.com>
2019-01-14 14:56:18 +0800

    Update jo2_find_longest_squece.py

--- LiDongYa/week12/jo2_find_longest_squece.py
+++ LiDongYa/week12/jo2_find_longest_squece.py
@@ -5,25 +5,25 @@
 '''
 
 
-# 方法一：利用浅拷贝知识。
+# 方法一：利用赋值引用性质。
 def find_longest_squece1(src):
     value_set = set()   # 存放元素
     value_dict = {}     # 存放元素和标记的键值对
     for i in src:
         if i not in value_set:
             value_set.add(i)    
-            value_dict[i] = [[1]]   # 添加标记记录连续序列的元素个数，利用shadow copy知识，一个标记改变，其他跟随改变
+            value_dict[i] = [1]   # 添加标记记录连续序列的元素个数，利用赋值引用的性质，一个标记改变，其他跟随改变
             if i+1 in value_set and i-1 in value_set:   # 当前后元素都存在时
-                value_dict[i+1][0][0] = value_dict[i+1][0][0] + 2  # 更新标记，元素个数增2
+                value_dict[i+1][0] = value_dict[i+1][0] + 2  # 更新标记，元素个数增2
                 value_dict[i] = value_dict[i+1]
                 value_dict[i-1] = value_dict[i+1]
             elif i-1 in value_set:  # 只存在前一个元素时
-                value_dict[i-1][0][0] = value_dict[i-1][0][0] + 1
+                value_dict[i-1][0] = value_dict[i-1][0] + 1
                 value_dict[i] = value_dict[i-1]
             elif i+1 in value_set:  # 只存在后一个元素时
-                value_dict[i+1][0][0] = value_dict[i+1][0][0] + 1
+                value_dict[i+1][0] = value_dict[i+1][0] + 1
                 value_dict[i] = value_dict[i+1]
-    return max(value_dict.values(), key=lambda x:x[0][0])[0][0]
+    return max(value_dict.values(), key=lambda x:x[0])[0]
 
 
 # 方法二：遍历元素，用序列最小值叠加1，判断，获得最长序列
